dc9fa3dd57b0e5f2643fef08f2d710ac71827e07
Asif Mujteba <asifmujteba@gmail.com>
2015-08-11 17:40:41 +0500

    project structure changes
    - renamed app to sample for clarity
    - fixed Activity transition glitch in sample

--- sample/src/main/java/com/github/asifmujteba/easyvolleysample/UI/activities/DetailsActivity.java
+++ sample/src/main/java/com/github/asifmujteba/easyvolleysample/UI/activities/DetailsActivity.java
@@ -1,11 +1,17 @@
 package com.github.asifmujteba.easyvolleysample.UI.activities;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
+import com.github.asifmujteba.easyvolley.ASFRequestListener;
 import com.github.asifmujteba.easyvolleysample.App;
 import com.github.asifmujteba.easyvolleysample.Models.Product;
 import com.github.asifmujteba.easyvolleysample.R;
@@ -32,9 +38,24 @@ public class DetailsActivity extends BaseActivity {
         }
 
         if (product.getImages().length > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                postponeEnterTransition();
+            }
+
             App.getInstance().getServerController().loadImage(product.getImages()[0].getPath())
                     .setScaleType(ImageView.ScaleType.CENTER_INSIDE)
                     .into(imageView)
+                    .setCallback(new ASFRequestListener<Bitmap>() {
+                        @Override
+                        public void onSuccess(Bitmap response) {
+                            scheduleStartPostponedTransition(imageView);
+                        }
+
+                        @Override
+                        public void onFailure(Exception e) {
+                            scheduleStartPostponedTransition(imageView);
+                        }
+                    })
                     .start();
         }
     }
@@ -49,11 +70,27 @@ public class DetailsActivity extends BaseActivity {
         return true;
     }
 
+    private void scheduleStartPostponedTransition(final View sharedElement) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+        sharedElement.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public boolean onPreDraw() {
+                        sharedElement.getViewTreeObserver().removeOnPreDrawListener(this);
+                        startPostponedEnterTransition();
+                        return true;
+                    }
+                });
+    }
+
     public static void launch(BaseActivity mContext, Product product, ImageView imageView) {
         Intent intent = new Intent(mContext, DetailsActivity.class);
         intent.putExtra(EXTRA_PRODUCT, Parcels.wrap(product));
         ActivityOptionsCompat options = ActivityOptionsCompat.
-                makeSceneTransitionAnimation(mContext, (View) imageView, "productImage");
-        mContext.startActivity(intent);
+                makeSceneTransitionAnimation(mContext, imageView, "productImage");
+        ActivityCompat.startActivity(mContext, intent, options.toBundle());
     }
 }
